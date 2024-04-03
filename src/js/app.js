1974d68b3eadcc452529b5e5e239e69bf2dbbed4
Ishafakhri <ishafakhri303@gmail.com>
2024-04-03 23:36:22 +0700

    Update script import paths and add event listener utility function

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,21 @@
  */
 
 'use strict';
+
+/**
+ * Module Import
+ */
+import { addEventOnElements } from 'utils.js';
+/**
+ * Toggle sidebar
+ */
+const /** {HTMLElement} */ $sidebar = document.querySelector('[data-sidebar]');
+
+const /** {Array<HTMLElement>} */ $sidebarTogglers = document.querySelector('[data-sidebar-toggler]');
+
+const /** {HTMLElement} */ $overlay = document.querySelector('[data-sidebar-overlay]');
+
+addEventOnElements($sidebarTogglers, 'click', function () {
+    $sidebar.classList.toggle('active');
+    $overlay.classList.toggle('active');
+});
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,23 @@
 /**
- * @copyright codewithsadee 2023
+ * @copyright ishafakhri 2024
  */
 
 'use strict';
+
+/**
+ * 
+ * @param {Array<HTMLElement></HTMLElement>} $elements 
+ * @param {string} eventType 
+ * @param {Function} callback 
+ */
+
+const addEventOnElements = function ($elements, eventType, callback) {
+    console.log($elements);
+    // $elements.forEach($element => {
+    //     $element.addEventListener(eventType, callback);
+    // });
+}
+
+export{
+    addEventOnElements
+}
