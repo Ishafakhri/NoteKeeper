4ca45ea4e55c7c77d69fc356b722729e2f2b1d27
Ishafakhri <ishafakhri303@gmail.com>
2024-04-05 16:59:02 +0700

    Refactor tooltip behavior in Tooltip.js

--- src/js/components/Tooltip.js
+++ src/js/components/Tooltip.js
@@ -9,6 +9,24 @@
  * @param {HTMLElement} $element 
  */
 export const Tooltip = function ($element){
-    console.log("🚀 ~ Tooltip ~ $element:", $element)
-    
+    const /** {HTMLElement} */ $tooltip = document.createElement('span');
+    $tooltip.classList.add('tooltip', 'text-body-small');
+    $element.addEventListener('mouseenter', function(){
+        $tooltip.textContent = this.dataset.tooltip;
+
+        const {
+            top,
+            left,
+            width,
+            height
+        } = this.getBoundingClientRect();
+        $tooltip.style.top = top + height + 4 + 'px';
+        $tooltip.style.left = left + (width/2) + 'px';
+        $tooltip.style.translateX = 'translate(-50%, 0)';
+        document.body.appendChild($tooltip);
+    });
+
+    $element.addEventListener('mouseleave', function(){
+        document.body.removeChild($tooltip);
+    });
 }
