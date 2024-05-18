fd726500088fbe633b2d67962066b423507adc2d
Ishafakhri <ishafakhri303@gmail.com>
2024-05-18 20:12:48 +0700

    Refactor Modal component to add delete confirmation functionality

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -1,5 +1,71 @@
 /**
- * @copyright codewithsadee 2023
+ * @copyright ishafakhri 2024
  */
 
 'use strict';
+
+const /** {HTMLElement} */ $overlay = document.createElement('div');
+$overlay.classList.add('overlay', 'modal-overlay');
+
+/**
+ * 
+ * @param {string} title
+ * @returns {Object} 
+ */
+
+const DeleteConfirmModal = function (title) { 
+    const /** {HTMLElement} */ $modal = document.createElement('div');
+    $modal.classList.add('modal');
+    $modal.innerHTML = `
+    
+      <h3 class="modal-title text-title-medium">
+        Are you sure want to delete this <strong>"${title}"</strong>?
+      </h3>
+      <div class="modal-footer">
+        <button class="btn text" data-action-btn="false">
+          <span class="text-label-large">Cancel</span>
+          <div class="state-layer"></div>
+        </button>
+        <button class="btn fill" data-action-btn="true">
+          <span class="text-label-large">Delete</span>
+          <div class="state-layer"></div>
+        </button>
+      </div>
+
+    `;
+
+    /**
+     * Opens the delete confirmation modal by appending it to the body
+     */
+    const open = function () { 
+        document.body.appendChild($modal);
+        document.body.appendChild($overlay);
+    }
+
+    /**
+     * closes the delete confirmation modal by removing it from the body
+     */
+    const close = function () {
+        document.body.appendChild($modal);
+        document.body.appendChild($overlay);
+    }
+
+    const /** {Array<HTMLElement></HTMLElement>}*/ $actionButton =
+    $modal.querySelectorAll('[data-action-btn]'); 
+
+
+
+    /**
+     * 
+     * @param {Function} callback 
+     */
+    const onSubmit = function (callback) {
+        $actionButton.forEach($btn => $btn.addEventListener('click', function () {
+                const /** {boolean} */ isDelete = this.dataset.actionBtn === 'true' ? true : false;
+                callback(isDelete);
+            }));
+    }
+
+    return {open, close, onSubmit};
+}
+export { DeleteConfirmModal };
--- src/js/components/NavItem.js
+++ src/js/components/NavItem.js
@@ -11,6 +11,7 @@ import { Tooltip } from "./Tooltip.js";
 import { activeNotebook, makeElemEditable } from "../utils.js";
 import { db } from "../db.js";
 import { client } from "../client.js";
+import { DeleteConfirmModal } from "./Modal.js";
 
 const /** {HTMLElement} */ $notePanelTitle = document.querySelector('[data-note-panel-title]');
 
@@ -65,6 +66,25 @@ export const NavItem = function (id, name) {
       //Render updated Notebook
       client.notebook.update(id, updateNotebookData);
     }
+  });
+
+  /**
+   * Notebook Delete Functionality
+   */
+  const /** {HTMLElement} */ $navItemDeleteBtn = $navItem.querySelector('[delete-data-btn]');
+  $navItemDeleteBtn.addEventListener('click', function () {
+    const /** {Object} */ modal = DeleteConfirmModal(name);
+
+    modal.open();
+
+    modal.onSubmit(function (isDelete) { 
+      if(isDelete){
+        //delete notebook from database
+        db.delete.notebook(id);
+      }
+
+      modal.close();
+    });
   });
 
   return $navItem;
